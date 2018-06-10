91e064e63393abc91965229b234b4561568d8552
dandeliono <dandeliono@users.noreply.github.com>
2018-06-10 23:35:52 +0800

    增加网页内容解析
    提取时间 解析网页获取正文

--- server/GSpider.py
+++ server/GSpider.py
@@ -10,22 +10,30 @@ headers = {
 
 def ajax():
     now = str(int(time.time()));
-    print(int(now))
-    contents = []
-    for i in range(1,10):
-        print(type(i))
+    print(int(now));
+    contents = [];
+    END = 2;
+    for i in range(1,END):
         page = str(i)
-        print(type(page))
         url = 'http://db2.gamersky.com/LabelJsonpAjax.aspx?callback=jQuery18309820627977549694_'+now+'&jsondata={"type":"updatenodelabel","isCache":true,"cacheTime":60,"nodeId":"11007","isNodeId":"true","page"' ':'+page+'}'
         request = urllib.request.Request(url, headers=headers)
         response = urllib.request.urlopen(request)
         content = response.read().decode('utf-8')
-
-        print(content)
         contents.append(content)
         print(contents)
         time.sleep(10);
     return contents
+#为方便编写 网页解析代码也同样放在这里
+def webPars(html):
+    request = urllib.request.Request(html, headers=headers)
+    response = urllib.request.urlopen(request)
+    content = response.read().decode('utf-8')
+    bodyPattern  = re.compile('<p.*?>(.*?)</p>');
+    bodyl = re.findall(bodyPattern, content)
+    print(bodyl)
+    body = "".join(bodyl)
+    print(body)
+
 def xkspider(url):
     contents = ajax()
     for content in contents:
@@ -34,14 +42,21 @@ def xkspider(url):
         items = re.findall(parttern,content)
         print(items);
         for item in items:
-            titPattern = re.compile('title="(.*?)"', re.S);
+            titPattern = re.compile('alt="(.*?)"', re.S);
             tit = re.findall(titPattern,item);
             title = "".join(tit);
             imgPattern = re.compile('<img src="(.*?)"',  re.S);
             image = re.findall(imgPattern, item)
             imgSrc = "".join(image)
+            jumpPattern = re.compile('<a class="tt" href="(.*?)"', re.S);
+            jumpA = re.findall(jumpPattern, item);
+            jumpUrl = "".join(jumpA);
+            relTiPattern = re.compile('class="time">(.*?)<',re.S);
+            relTi = re.findall(relTiPattern, item);
+            relTime = "".join(relTi);
             if(imgSrc):
-                #print(item);
-                print("title:"+title+"imgSrc:"+imgSrc)
+                print(item);
+                print("title:"+title+"imgSrc:"+imgSrc+" jumpUrl:"+jumpUrl+" time:"+relTime);
+                webPars(jumpUrl);
 xkspider(url)
 #ajax();
